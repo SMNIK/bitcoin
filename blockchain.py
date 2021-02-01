b5af95327bc95f2bd0cd294ecb343042f5e013e6
SMNIK <masood.milestone@yahoo.com>
2021-01-31 17:40:20 -0800

    Create blockchain.py

---
+++ blockchain.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 16:33:10 2021
+
+@author: masou
+"""
+
+
+class Blockchain(object):
+    ''' define a block chain on one machine '''
+
+    def __init__(self):
+        self.chain = []
+        self.current_trxs = []
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        ''' creat a new block '''
+        block = {'index': len(self.chain) + 1, 'timestamp': time(), 'trxs': self.current_trxs,
+                 'proof': proof, 'previous_hash': previous_hash or self.hash(self.chain[-1]), }
+
+    def new_trx(self, sender, recipient, amount):
+        ''' add a new trxs to the mempool '''
+        self.current_trxs.append(
+            {'sender': sender, 'recipient': recipient, 'amount': amount})
+
+    @staticmethod
+    def hash(block):
+        ''' hash a block '''
+        pass
+
+    @property
+    def last_block(self):
+        ''' return last block '''
+        pass
